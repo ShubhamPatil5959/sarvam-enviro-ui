3eb82b35b400cd685c6301005db32869622e6dc3
ShubhamPatil5959 <sp4181724@gmail.com>
2026-05-04 22:32:26 +0530

    integrste servives part

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from 'react-router-dom';
-import { Leaf, Menu, X } from 'lucide-react';
+import { Menu, X } from 'lucide-react';
 import { useState, useEffect } from 'react';
 
 export default function Navbar() {
